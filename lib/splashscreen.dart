db11bbb53afddc380b2d95370d5655cd4bda0ff7
Gowshik Prabhu <gowshikprabhu@gmail.com>
2020-09-22 13:51:45 +0530

    add splash screen

---
+++ lib/splashscreen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+// import 'package:imageclassifier/home.dart';
+import 'package:splashscreen/splashscreen.dart';
+
+class MySplashScreen extends StatefulWidget {
+  @override
+  _MySplashScreenState createState() => _MySplashScreenState();
+}
+
+class _MySplashScreenState extends State<MySplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SplashScreen(
+      seconds: 2,
+      // navigateAfterSeconds: Home(),
+      title: Text(
+        'Fruits and Vegetables Recognizer',
+        style: TextStyle(
+            fontWeight: FontWeight.bold, fontSize: 30, color: Colors.white),
+      ),
+      image: Image.asset('assets/bgimg.png'),
+      backgroundColor: Colors.black,
+      photoSize: 100,
+      loaderColor: Color(0xFF1E90FF),
+    );
+  }
+}
